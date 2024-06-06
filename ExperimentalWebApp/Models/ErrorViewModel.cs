6791fc639228947f3e98b209d3654f69606b3ead
quocb <quocb@QUOCBUI>
2024-06-06 15:15:49 -0500

    Add project files.

---
+++ ExperimentalWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ExperimentalWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
