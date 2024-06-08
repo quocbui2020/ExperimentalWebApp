c596491652d4caa3ec3bafd8c01cbf92b727071d
quocbui2020 <quocbui2010@yahoo.com>
2024-06-08 08:34:05 -0500

    Set up WebApi and different appsettings.

--- ExperimentalWebApp/Controllers/HomeController.cs
+++ ExperimentalWebApp/Controllers/HomeController.cs
@@ -7,14 +7,17 @@ namespace ExperimentalWebApp.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         public IActionResult Index()
         {
+            ViewBag.Connection = _configuration["ConnectionString"];
             return View();
         }
 
